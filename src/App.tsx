4c8de53fa097b4db1aa2f7e100576a2104561414
ramiyohay <ramiyohay1@gmail.com>
2022-06-17 11:36:31 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+        <Login />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login.tsx
@@ -0,0 +1,66 @@
+import { Password } from "primereact/password";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { Fieldset } from "primereact/fieldset";
+import { BlockUI } from "primereact/blockui";
+import React, { useState } from "react";
+import { preProcessFile } from "typescript";
+
+function Login() {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading1, setLoading1] = useState(false);
+  const [blockedPanel, setBlockedPanel] = useState<boolean>(false);
+
+  const handleClick = () => {
+    console.log(password);
+    setLoading1(true);
+    setBlockedPanel(true);
+  };
+
+  return (
+    <>
+      <BlockUI
+        blocked={blockedPanel}
+        template={<i className="pi pi-lock" style={{ fontSize: "3rem" }} />}
+      >
+        <Fieldset
+          legend={
+            "(Login " +
+            process.env.NODE_ENV +
+            " " +
+            process.env.REACT_APP_APP_VER +
+            ")"
+          }
+        >
+          <h5>User</h5>
+          <span className="p-input-icon-left">
+            <i className="pi pi-user" />
+            <InputText
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+              placeholder="user name"
+            />
+          </span>
+
+          <h5>Password</h5>
+          <Password
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            toggleMask
+          />
+        </Fieldset>
+      </BlockUI>
+
+      <Button
+        label="Submit"
+        loading={loading1}
+        className="p-button-info"
+        onClick={handleClick}
+        style={{ marginTop: "10px" }}
+      />
+    </>
+  );
+}
+
+export default Login;
